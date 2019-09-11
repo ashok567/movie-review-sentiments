a50352c9d284d2615268568c69b8a6f0c94100a4
Ashok Thakur <ashokchand.thakur@gramener.com>
2019-09-11 22:14:35 +0530

    review scraping issue fixed

--- scrap_data.py
+++ scrap_data.py
@@ -15,15 +15,26 @@ time.sleep(5)
 dataset = pd.DataFrame(columns=[['date', 'user', 'rating', 'review']])
 
 for i in range(1, 51):
+
+    review_xpath = '//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div[1]/div[1]/div[3]/div[1]'
+    expand_xpath = '//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[4]/div/div'
+    if driver.find_elements_by_xpath(expand_xpath):
+        review_expand_button = driver.find_elements_by_xpath(expand_xpath)[0]
+        review_expand_button.click()
+        print('Expand button clicked')
+        review_xpath = '//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[4]/div[1]'
+
     rating = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[1]/span/span[1]')[0]
 
     user = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[2]/span[1]')[0]
 
     date = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[2]/span[2]')[0]
 
-    review = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/a')[0]
+    # review = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/a')[0]
+
+    review = driver.find_elements_by_xpath(review_xpath)[0]
 
     dataset.loc[len(dataset)] = [date.text, user.text, rating.text, review.text.strip()]
 
-dataset.to_csv('review.csv', index=False)
+dataset.to_csv('data/review.csv', index=False)
 driver.quit()
