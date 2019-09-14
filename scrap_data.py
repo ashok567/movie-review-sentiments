6a1a9ace920ebf00de3e66095326249c6cd6804c
Ashok Thakur <ashokchand.thakur@gramener.com>
2019-09-14 20:40:12 +0530

    scrapped data changed

--- scrap_data.py
+++ scrap_data.py
@@ -8,13 +8,14 @@ driver = webdriver.Chrome()
 driver.get(url)
 
 loadMoreButton = driver.find_element_by_xpath('//*[@id="load-more-trigger"]')
-print('Load more button clicked')
-loadMoreButton.click()
-time.sleep(5)
 
-dataset = pd.DataFrame(columns=[['date', 'user', 'rating', 'review']])
+dataset = pd.DataFrame(columns=[['date', 'user', 'review']])
 
-for i in range(1, 51):
+for i in range(1, 201):
+    if i % 25 == 0:
+        loadMoreButton.click()
+        print('Load more button clicked')
+        time.sleep(2)
 
     review_xpath = '//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div[1]/div[1]/div[3]/div[1]'
     expand_xpath = '//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[4]/div/div'
@@ -24,17 +25,14 @@ for i in range(1, 51):
         print('Expand button clicked')
         review_xpath = '//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[4]/div[1]'
 
-    rating = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[1]/span/span[1]')[0]
-
-    user = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[2]/span[1]')[0]
+    test = '//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[2]/span[1]'
+    user = driver.find_elements_by_xpath(test)[0]
 
     date = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/div[2]/span[2]')[0]
 
-    # review = driver.find_elements_by_xpath('//*[@id="main"]/section/div[2]/div[2]/div['+str(i)+']/div/div[1]/a')[0]
-
     review = driver.find_elements_by_xpath(review_xpath)[0]
 
-    dataset.loc[len(dataset)] = [date.text, user.text, rating.text, review.text.strip()]
+    dataset.loc[len(dataset)] = [date.text, user.text, review.text.strip()]
 
 dataset.to_csv('data/review.csv', index=False)
 driver.quit()
